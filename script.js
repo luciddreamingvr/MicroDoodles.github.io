4bc231c98115ef495f8deb4de0bdb1211f84eb02
FreeRoamGames <58123694+FreeRoamGames@users.noreply.github.com>
2025-04-08 20:37:29 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
- var dosbox = new Dosbox({
-        id: "dosbox",
-        onload: function (dosbox) {   dosbox.run("https://js-dos.com/cdn/upload/DOOM-@evilution.zip", "./DOOM/DOOM.EXE");
-        },
-        onrun: function (dosbox, app) {
-          console.log("App '" + app + "' is runned");
-        }
-      });
